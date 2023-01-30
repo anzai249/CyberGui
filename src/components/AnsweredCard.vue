9650744ac7766f8efa0b3b47790ba3fb1cbb0247
An <a381811169@163.com>
2023-01-30 20:12:43 +0800

    Answered Card

---
+++ src/components/AnsweredCard.vue
@@ -0,0 +1,178 @@
+<script setup>
+defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    msg: {
+        type: String,
+        required: true
+    },
+    likes: {
+        type: Number,
+        required: true
+    },
+    dislikes: {
+        type: Number,
+        required: true
+    },
+    time: {
+        type: String,
+        required: true
+    },
+    loading: {
+        type: Boolean,
+        required: true
+    },
+    sensitive: {
+        type: Boolean,
+        required: true
+    },
+    answer: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import { Person, Heart, HeartDislike } from '@vicons/ionicons5'
+
+export default defineComponent({
+    props: {
+        pointerEve: {
+            type: String
+        },
+        blurRate: {
+            type: String
+        }
+    },
+    components: {
+        Person, Heart, HeartDislike
+    },
+    data() {
+        if (this.sensitive) {
+            return {
+                blurRate: '5px',
+                pointerEve: 'none',
+                sensiSwitch: true
+            }
+        } else {
+            return {
+                blurRate: '0px',
+                pointerEve: 'auto',
+                sensiSwitch: false
+            }
+        }
+    },
+    setup() {
+
+    },
+    methods: {
+        showSensi() {
+            console.log('123')
+            this.blurRate = '0px'
+            this.pointerEve = 'suto'
+            this.sensiSwitch = false
+        }
+    }
+})
+</script>
+
+<style>
+.mask {
+    position: absolute;
+    width: 100%;
+    left: 0;
+    top: 0;
+    filter: blur(v-bind(blurRate));
+    pointer-events: v-bind(pointerEve);
+}
+
+.cardAll {
+    position: absolute;
+    display: flex;
+    width: 100%;
+    height: 250px;
+    left: 0;
+    top: 0;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+}
+</style>
+
+<template>
+    <div class="cardAll">
+        <div class="mask" ref="mask">
+            <n-space vertical>
+                <n-card ref="card">
+                    <template #header>
+                        <n-divider v-if="loading" title-placement="left" style="top: -10px;">
+                            <n-skeleton v-if="loading" height="34px" circle />
+                        </n-divider>
+                        <n-skeleton v-if="loading" text width="60%" height="20px" />
+                        <template v-else>
+                            <n-divider title-placement="left" style="top: -10px;">
+                                <n-avatar round>
+                                    <n-icon>
+                                        <person />
+                                    </n-icon>
+                                </n-avatar>
+                            </n-divider>
+                            <div>
+                                {{ title }}
+                            </div>
+                        </template>
+                    </template>
+                    <n-space vertical>
+                        <n-skeleton v-if="loading" text :repeat="4" />
+                        <template v-else>
+                            <div class="question_div" style="word-wrap:break-word;">
+                                {{ msg }}
+                            </div>
+                        </template>
+                        <n-space vertical>
+                            <n-space justify="space-between" size="large" style="height: 28px;">
+                                <n-skeleton v-if="loading" :width="75" text style="top:15%; position: relative;" />
+                                <div v-else style="color: rgba(204, 204, 204, 1); top:15%; position: relative;">
+                                    {{ time }}
+                                </div>
+                                <n-space justify="end">
+                                    <n-skeleton v-if="loading" :width="90" round size="small" />
+                                    <n-button v-else style="font-size: 20px; width:90px" round size="small">
+                                        <n-icon>
+                                            <heart />
+                                        </n-icon>
+                                        {{ likes }}
+                                    </n-button>
+                                    <n-skeleton v-if="loading" :width="90" round size="small" />
+                                    <n-button v-else style="font-size: 20px; width:90px" round size="small">
+                                        <n-icon>
+                                            <HeartDislike />
+                                        </n-icon>
+                                        {{ dislikes }}
+                                    </n-button>
+                                </n-space>
+                            </n-space>
+                            <n-divider title-placement="left">
+                                <n-skeleton v-if="loading" height="34px" circle />
+                                <n-avatar v-else round src="./src/assets/Avatar.jpg" />
+                            </n-divider>
+                            <n-skeleton v-if="loading" text :repeat="3" />
+                        <template v-else>
+                            <div class="answer_div" style="word-wrap:break-word;">
+                                {{ answer }}
+                            </div>
+                        </template>
+                        </n-space>
+                    </n-space>
+                </n-card>
+            </n-space>
+        </div>
+        <n-button v-if="sensiSwitch" strong secondary type="primary" @click="showSensi()">
+            {{ $t('card.showSensitive') }}
+        </n-button>
+    </div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -17,9 +17,14 @@
   </n-switch>
   UNDER DEVELOPMENT
   <div style="width: 50%; left: 25%; position: fixed;">
-    <UnansweredCard title="石室诗士施氏"
-      msg="石室诗士施氏，嗜狮，誓食十狮。适施氏时时适市视狮。十时，适十狮适市。是时，适施氏适市。氏视是十狮，恃矢势，使是十狮逝世。氏拾是十狮尸，适石室。石室湿，氏使侍拭石室。石室拭，氏始试食是十狮尸。食时，始识是十狮尸，实十石狮尸。试释是事。"
-      :likes="666" :dislikes="994" time="2023-01-29" :loading="loading" :sensitive="sensitive" />
+    <n-space vertical>
+      <!-- <UnansweredCard title="石室诗士施氏"
+        msg="石室诗士施氏，嗜狮，誓食十狮。适施氏时时适市视狮。十时，适十狮适市。是时，适施氏适市。氏视是十狮，恃矢势，使是十狮逝世。氏拾是十狮尸，适石室。石室湿，氏使侍拭石室。石室拭，氏始试食是十狮尸。食时，始识是十狮尸，实十石狮尸。试释是事。"
+        :likes="666" :dislikes="994" time="2023-01-29" :loading="loading" :sensitive="sensitive" /> -->
+      <AnsweredCard title="石室诗士施氏"
+        msg="石室诗士施氏，嗜狮，誓食十狮。适施氏时时适市视狮。十时，适十狮适市。是时，适施氏适市。氏视是十狮，恃矢势，使是十狮逝世。氏拾是十狮尸，适石室。石室湿，氏使侍拭石室。石室拭，氏始试食是十狮尸。食时，始识是十狮尸，实十石狮尸。试释是事。"
+        :likes="666" :dislikes="994" time="2023-01-29" :loading="loading" :sensitive="sensitive" answer="季姬寂，集鸡，鸡即棘鸡。棘鸡饥叽，季姬及箕稷济鸡。鸡既济，跻姬笈，季姬忌，急咭鸡，鸡急，继圾几，季姬急，即籍箕击鸡，箕疾击几伎，伎即齑，鸡叽集几基，季姬急极屐击鸡，鸡既殛，季姬激，即记《季姬击鸡记》。" />
+    </n-space>
   </div>
 
 </template>
@@ -28,10 +33,11 @@
 import { defineComponent, ref } from 'vue'
 import { Person, Heart, HeartDislike } from '@vicons/ionicons5'
 import UnansweredCard from '../components/UnansweredCard.vue'
+import AnsweredCard from '../components/AnsweredCard.vue'
 
 export default defineComponent({
   components: {
-    Person, Heart, HeartDislike, UnansweredCard
+    Person, Heart, HeartDislike, UnansweredCard, AnsweredCard
   },
   setup() {
 
