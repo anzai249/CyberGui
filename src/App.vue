71a486ad1ad63e1624d65ccccb4ea4bd7b546ed5
An <a381811169@163.com>
2023-02-01 17:03:18 +0800

    scrolling

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,50 @@
 <template>
   <n-config-provider :theme-overrides="themeOverrides">
-    <NLayout class="layout" :position="fixedMenu">
-      <NLayout>
-        <NLayoutHeader :inverted="getHeaderInverted" :position="fixedHeader">
-          <n-tabs :bar-width="35" type="line" animated size="large">
-            <template #prefix>
-              <n-divider vertical />
-              <img width="150" src="@/assets/logo.png" />
-              <n-divider vertical />
-            </template>
-            <n-tab-pane name="archive" :tab="$t('header.archive')">
-            </n-tab-pane>
-            <n-tab-pane name="about" :tab="$t('header.about')">
-            </n-tab-pane>
-            <template #suffix>
-              <n-button ghost color="#8a2be2" @click="active = true">
-                <template #icon>
-                  <n-icon>
-                    <add-icon />
-                  </n-icon>
-                </template>
-                {{ $t('header.addNew') }}
-              </n-button>
-              <n-divider vertical />
-              <n-space vertical style="width:175px">
-                <n-select v-model:value="value" :options="options" :render-label="renderLabel"
-                  :render-tag="renderSingleSelectTag" placeholder="Language" @update:value="changeLangEvent" />
-              </n-space>
-              <n-divider vertical />
-            </template>
-          </n-tabs>
-          <PageHeader v-model:collapsed="collapsed" :inverted="inverted" />
-        </NLayoutHeader>
-        <NLayoutContent>
-          <RouterView style="width:50%; align-self: center; left: 25%; position: fixed;" />
-        </NLayoutContent>
-      </NLayout>
-    </NLayout>
+    <n-layout position="absolute">
+      <n-layout-header style="height: 53px;" bordered>
+        <n-tabs :bar-width="35" type="line" animated size="large">
+          <template #prefix>
+            <n-divider vertical />
+            <img width="150" src="@/assets/logo.png" />
+            <n-divider vertical />
+          </template>
+          <n-tab-pane name="archive" :tab="$t('header.archive')">
+          </n-tab-pane>
+          <n-tab-pane name="about" :tab="$t('header.about')">
+          </n-tab-pane>
+          <template #suffix>
+            <n-button ghost color="#8a2be2" @click="active = true">
+              <template #icon>
+                <n-icon>
+                  <add-icon />
+                </n-icon>
+              </template>
+              {{ $t('header.addNew') }}
+            </n-button>
+            <n-divider vertical />
+            <n-space vertical style="width:175px">
+              <n-select v-model:value="value" :options="options" :render-label="renderLabel"
+                :render-tag="renderSingleSelectTag" placeholder="Language" @update:value="changeLangEvent" />
+            </n-space>
+            <n-divider vertical />
+          </template>
+        </n-tabs>
+      </n-layout-header>
+      <n-layout position="absolute"  style="top: 53px; bottom: 0px">
+        <RouterView />
+      </n-layout>
+    </n-layout>
     <n-drawer v-model:show="active" :width="500" placement="right">
       <n-drawer-content :title="$t('header.addNew')">
         <n-form>
           <n-form-item>
             <div style="left: 50%; width: auto;">
-              <n-space vertical align="center" >
+              <n-space vertical align="center">
                 <n-input round v-model:title="title" :placeholder="$t('addNew.title')" show-count :maxlength="12"
-                  :count-graphemes="countGraphemes" style="width: 450px;"/>
-                <n-input round v-model:detail="detail" type="textarea" :placeholder="$t('addNew.detail')" style="width: 450px;"/>
-                <n-switch v-model:sensitive="sensitive" >
+                  :count-graphemes="countGraphemes" style="width: 450px;" />
+                <n-input round v-model:detail="detail" type="textarea" :placeholder="$t('addNew.detail')"
+                  style="width: 450px;" />
+                <n-switch v-model:sensitive="sensitive">
                   <template #checked>
                     {{ $t('addNew.sensitiveTrue') }}
                   </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div style="width: 50%; left: 25%; position: fixed; height: 100%;">
+  <n-layout>
+    <n-layout-content content-style="padding: 30px; width: 60%;" style="justify-content: center; display: flex;">
       <n-switch v-model:value="loading">
         <template #checked>
           Skel
@@ -21,7 +21,7 @@
       <n-space vertical>
         <AnsweredCard title="石室诗士施氏"
           msg="石室诗士施氏，嗜狮，誓食十狮。适施氏时时适市视狮。十时，适十狮适市。是时，适施氏适市。氏视是十狮，恃矢势，使是十狮逝世。氏拾是十狮尸，适石室。石室湿，氏使侍拭石室。石室拭，氏始试食是十狮尸。食时，始识是十狮尸，实十石狮尸。试释是事。"
-          :likes="666" :dislikes="994" time="2023-01-29" :loading="loading" :sensitive="sensitive"
+          :likes="666" :dislikes="994" time="2023-01-29" :loading="loading" :sensitive="false"
           answer="季姬寂，集鸡，鸡即棘鸡。棘鸡饥叽，季姬及箕稷济鸡。鸡既济，跻姬笈，季姬忌，急咭鸡，鸡急，继圾几，季姬急，即籍箕击鸡，箕疾击几伎，伎即齑，鸡叽集几基，季姬急极屐击鸡，鸡既殛，季姬激，即记《季姬击鸡记》。" />
         <UnansweredCard title="石室诗士施氏"
           msg="石室诗士施氏，嗜狮，誓食十狮。适施氏时时适市视狮。十时，适十狮适市。是时，适施氏适市。氏视是十狮，恃矢势，使是十狮逝世。氏拾是十狮尸，适石室。石室湿，氏使侍拭石室。石室拭，氏始试食是十狮尸。食时，始识是十狮尸，实十石狮尸。试释是事。"
@@ -31,8 +31,11 @@
           :likes="666" :dislikes="994" time="2023-01-29" :loading="loading" :sensitive="sensitive"
           answer="季姬寂，集鸡，鸡即棘鸡。棘鸡饥叽，季姬及箕稷济鸡。鸡既济，跻姬笈，季姬忌，急咭鸡，鸡急，继圾几，季姬急，即籍箕击鸡，箕疾击几伎，伎即齑，鸡叽集几基，季姬急极屐击鸡，鸡既殛，季姬激，即记《季姬击鸡记》。" />
       </n-space>
-    </div>
-  </div>
+    </n-layout-content>
+    <n-layout-footer class="innerFooter">
+
+    </n-layout-footer>
+  </n-layout>
 </template>
   
 <script lang="ts">
@@ -59,5 +62,10 @@ export default defineComponent({
 <style>
 body {
   background-color: snow;
+}
+
+.innerFooter {
+  background: rgba(128, 128, 128, 0.2);
+  padding: 24px;
 }
 </style>
