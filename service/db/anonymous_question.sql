b303b1645649fb7afa72ea11f1f150670cfcc8dd
anzai249 <a381811169@163.com>
2023-07-24 21:58:44 +0800

    new sql file

--- service/db/anonymous_question.sql
+++ service/db/anonymous_question.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80028 (8.0.28)
  File Encoding         : 65001
 
- Date: 30/01/2023 19:31:35
+ Date: 24/07/2023 21:57:43
 */
 
 SET NAMES utf8mb4;
@@ -38,16 +38,20 @@ CREATE TABLE `answers`  (
 DROP TABLE IF EXISTS `questions`;
 CREATE TABLE `questions`  (
   `id` int(8) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
-  `time` datetime NULL DEFAULT NULL,
-  `question` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `time` datetime NOT NULL,
+  `question` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `like` int NOT NULL DEFAULT 0,
   `dislike` int NOT NULL DEFAULT 0,
   `sensitive` tinyint NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+  `answerid` int(8) UNSIGNED ZEROFILL NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `answerid`(`answerid` ASC) USING BTREE,
+  CONSTRAINT `questions_ibfk_1` FOREIGN KEY (`answerid`) REFERENCES `answers` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of questions
 -- ----------------------------
+INSERT INTO `questions` VALUES (00000001, '2023-07-24 13:29:16', '你好', 10, 5, 0, NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
