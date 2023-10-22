93193b5dcec84118009a5669d7ffa35f5621015e
anzai249 <a381811169@163.com>
2023-10-22 17:20:20 +0800

    NodeRSA$2 is not a constructor

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { viteRequire } from 'vite-require'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), viteRequire(), nodePolyfills()],
+  plugins: [vue(), viteRequire(), nodePolyfills(),
+  topLevelAwait({
+    // The export name of top-level await promise for each chunk module
+    promiseExportName: '__tla',
+    // The function to generate import names of top-level await promise in each chunk module
+    promiseImportName: i => `__tla_${i}`
+  })],
   base: './',
   resolve: {
     alias: {
@@ -15,6 +22,22 @@ export default defineConfig({
     }
   },
   server: {
+    port: 1108,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:1107',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      '/ws': {
+        target: 'http://localhost:1106',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/ws/, ''),
+        ws: true
+      }
+    }
+  },
+  preview: {
     port: 1108,
     proxy: {
       '/api': {
