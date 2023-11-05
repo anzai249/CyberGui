779e536b61fcda3664ce258b1e4dae602d476aab
anzai249 <a381811169@163.com>
2023-11-05 20:19:21 +0800

    session bug fix

--- service/modules/session.js
+++ service/modules/session.js
@@ -6,6 +6,11 @@ const { getUTCDate, getUTCTime } = require('../modules/date.js');
 async function checkSession(session, mysql) {
     const savedSession = await mysql.query("SELECT `session` FROM `sid` WHERE `id` = 1");
     const savedTime = await mysql.query("SELECT `time` FROM `sid` WHERE `id` = 1");
+
+    if (savedSession.length === 0 || savedTime.length === 0) {
+        return false;
+    }
+
     const givenSession = session;
     const dateTime = savedTime[0].time
 
