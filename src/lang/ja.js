5c638c05c9727f3515355a1ada07c49a5f9e4b21
boundary <boundary6@icloud.com>
2025-01-06 20:27:32 +0900

    Add japanese translation

--- src/lang/ja.js
+++ src/lang/ja.js
@@ -7,35 +7,40 @@ export default {
     },
     addNew: {
         title: "タイトル",
-        detail: "内容",
-        sensitiveTrue: "機密情報を含む",
-        sensitiveFalse: "機密情報は含まれていません",
-        submit: "提出",
+        detail: "內容",
+        sensitiveTrue: "敏感な內容を含む",
+        sensitiveFalse: "敏感な內容は含まない",
+        submit: "送信",
         success: "質問が成功しました！",
-        error: "エラー："
+        error: "エラー：",
+        needReview: "あなたの質問はサイトの管理者による承認が必要です。承認後に表示されます。"
     },
     card: {
-        showSensitive: "機密情報を表示"
+        showSensitive: "敏感な內容を表示"
     },
     aboutPage: {
-        description: "CyberGuiはオープンソースの匿名の個人質問箱であり、クリーンで簡単に展開でき、匿名の質問を通じてあなたを理解する手段を提供します。",
-        guiCharacter: "匦は古代中国の政府が人々からの投書を収集するために使用した箱です。",
+        description: "CyberGuiはオープンソースの個人向け匿名質問箱であり、クリーンで簡単にデプロイできるソフトウェアです。匿名質問を通じて他の人があなたを知ることができます。",
+        guiCharacter: "匭（くい）は中国古代において政府が人々の投書を集めるために使用した箱です。",
         author: "作者：",
         github: "GitHub：",
-        email: "Eメール："
+        email: "メールアドレス："
     },
+
     terminal: {
         answer: "あなたの回答",
         delete: "削除",
-        sensitiveTrue: "機密情報",
+        sensitiveTrue: "敏感",
         sensitiveFalse: "通常",
         login: "ログイン",
         confirm: "確認",
         password: "パスワード",
         wrongPass: "エラー：",
-        emptyPass: "空のパスワード",
-        deleteConfirm: "削除しますか？",
+        emptyPass: "パスワードが空です",
+        deleteConfirm: "削除してもよろしいですか？",
         cancel: "キャンセル",
-        deleted: "削除されました"
+        deleted: "削除しました",
+        answerquestion: "回答する",
+        review: "審査",
+        success: "操作が成功しました"
     }
 }
